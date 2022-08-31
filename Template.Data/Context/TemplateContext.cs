9cb2ede48af674025b1f29232e411d988a93ad69
sergio moraes <sergiomoraes.rc@gmail.com>
2022-08-31 00:45:16 -0300

    Backend Structure Database

---
+++ Template.Data/Context/TemplateContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Data.Mappings;
+using Template.Domain.Entities;
+
+namespace Template.Data.Context
+{
+    public class TemplateContext : DbContext
+    {
+        public TemplateContext(DbContextOptions<TemplateContext> options)
+            : base(options) { }
+
+        #region "DBSets"
+        public DbSet<User> User { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
